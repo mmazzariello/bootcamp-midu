6b3c47b3d829844071dfaccdd70cff3ce65604e7
Mariana Mazzariello <mfmazzariello@gmail.com>
2022-04-27 10:45:05 +0200

    ADD counter with useState hook

--- part-1/courseinfo/src/index.js
+++ part-1/courseinfo/src/index.js
@@ -1,6 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const Header = ({ course }) => <h1>{course}</h1>;
+const Total = (props) => {
+  <div>
+    <p>
+      {" "}
+      {props.exercises1} + {props.exercises2} + {props.exercises3}
+    </p>
+  </div>;
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </div>
+  );
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -10,19 +37,32 @@ const App = () => {
   const part3 = "State of a component";
   const exercises3 = 14;
 
+  const [contador, setContador] = React.useState(0);
+
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} />
+      <Content part2={part2} exercises2={exercises2} />
+      <Content part3={part3} exercises3={exercises3} />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+
+      <button
+        onClick={() => {
+          setContador(contador - 1);
+        }}
+      >
+        Decrement
+      </button>
+      <span>{contador}</span>
+      <button
+        onClick={() => {
+          setContador(contador + 1);
+        }}
+      >
+        {" "}
+        Increment
+      </button>
     </div>
   );
 };
